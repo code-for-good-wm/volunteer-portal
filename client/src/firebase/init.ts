97674ed118e2602033bf0f88cec4054998d3e0c3
Sloth I <github@slothwerks.com>
2022-05-18 12:26:29 -0400

    Added Firebase

---
+++ client/src/firebase/init.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FB_API_KEY,
+  authDomain: process.env.REACT_APP_FB_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FB_APP_ID,
+};
+
+// Initialize Firebase
+export const fbApp = initializeApp(firebaseConfig);
