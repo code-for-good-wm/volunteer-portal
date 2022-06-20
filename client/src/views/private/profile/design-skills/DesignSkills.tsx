317f3f0bed6c6cf1d1d7b09fdfc86576470b0d67
Sloth I <github@slothwerks.com>
2022-06-20 14:26:33 -0400

    Added Design Skills view content

--- client/src/views/private/profile/design-skills/DesignSkills.tsx
+++ client/src/views/private/profile/design-skills/DesignSkills.tsx
@@ -1,11 +1,344 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { useAppSelector, useAppDispatch } from '../../../../store/hooks';
+import { user } from '../../../../store/authSlice';
+import { updateProfile } from '../../../../store/profileSlice';
+
+import { ProfileSkill, UserSkill } from '../../../../types/profile';
 
 import ProfileLayout from '../../../../layouts/ProfileLayout';
 
+import StandardButton from '../../../../components/buttons/StandardButton';
+import SkillCard from '../../../../components/elements/SkillCard';
+
+import { convertSkillDataToObject, getNextProfileSection } from '../../../../helpers/functions';
+import { getUserSkills, updateUserSkills } from '../../../../services/profile';
+import { skillLevels, designSkills } from '../../../../helpers/constants';
+
 const DesignSkills = () => {
+  const [experienceLevels, setExperienceLevels] = useState<ProfileSkill[]>([]);
+  
+  const [tools, setTools] = useState<ProfileSkill[]>([]);
+
+  const [development, setDevelopment] = useState<ProfileSkill[]>([]);
+
+  const [processing, setProcessing] = useState(false);
+
+  const userData = useAppSelector(user);
+
+  const dispatch = useAppDispatch();
+
+  const navigate = useNavigate();
+
+  // On mount, update the current profile section
+  useEffect(() => {
+    dispatch(
+      updateProfile({
+        currentSection: 'design-skills',
+      })
+    );
+  }, []);
+
+  // On mount, pull any current profile data and populate the forms
+  useEffect(() => {
+    const userSkills = getUserSkills() ?? [];
+
+    // Build object out of user skill data
+    const userSkillData = convertSkillDataToObject(userSkills);
+
+    const populatedExperienceLevels = designSkills.experienceLevel.map((skill) => {
+      const { code, description } = skill;
+      const merge: ProfileSkill = {
+        code,
+        description,
+        level: userSkillData[code] ?? 0,
+      };
+      return merge;
+    });
+
+    const populatedTools = designSkills.tools.map((skill) => {
+      const { code, description } = skill;
+      const merge: ProfileSkill = {
+        code,
+        description,
+        level: userSkillData[code] ?? 0,
+      };
+      return merge;
+    });
+
+    const populatedDevelopment = designSkills.development.map((skill) => {
+      const { code, description } = skill;
+      const merge: ProfileSkill = {
+        code,
+        description,
+        level: userSkillData[code] ?? 0,
+      };
+      return merge;
+    });
+
+    setExperienceLevels(populatedExperienceLevels);
+    setTools(populatedTools);
+    setDevelopment(populatedDevelopment);
+  }, []);
+
+  const handleExperienceLevelUpdate = (skillData: UserSkill) => {
+    const { code, level } = skillData;
+
+    const update = experienceLevels.map((setting) => {
+      if (setting.code === code) {
+        const newSetting = {
+          ...setting,
+          level,
+        };
+        return newSetting;
+      }
+      return setting;
+    });
+
+    setExperienceLevels(update);
+  };
+
+  const handleToolsUpdate = (skillData: UserSkill) => {
+    const { code, level } = skillData;
+
+    const update = tools.map((setting) => {
+      if (setting.code === code) {
+        const newSetting = {
+          ...setting,
+          level,
+        };
+        return newSetting;
+      }
+      return setting;
+    });
+
+    setTools(update);
+  };
+
+  const handleDevelopmentUpdate = (skillData: UserSkill) => {
+    const { code, level } = skillData;
+
+    const update = development.map((setting) => {
+      if (setting.code === code) {
+        const newSetting = {
+          ...setting,
+          level,
+        };
+        return newSetting;
+      }
+      return setting;
+    });
+
+    setDevelopment(update);
+  };
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  const handleNext = () => {
+    // TODO: Replace with actual user update functionality
+    if (userData) {
+      setProcessing(true);
+
+      const skillUpdate: UserSkill[] = [];
+
+      experienceLevels.forEach((setting) => {
+        const { code, level } = setting;
+        skillUpdate.push({
+          code,
+          level
+        });
+      });
+
+      tools.forEach((setting) => {
+        const { code, level } = setting;
+        skillUpdate.push({
+          code,
+          level
+        });
+      });
+
+      development.forEach((setting) => {
+        const { code, level } = setting;
+        skillUpdate.push({
+          code,
+          level
+        });
+      });
+
+      const updateResult = updateUserSkills(skillUpdate);
+
+      if (!updateResult) {
+        // TODO: Handle errors
+      }
+
+      setProcessing(false);
+    }
+
+    // Determine next view to display
+    const nextSection = getNextProfileSection() ?? '';
+    if (!nextSection) {
+      // TODO: If something bad happens here, what do we do?
+      return;
+    }
+    if (nextSection === true) {
+      navigate('/profile/complete');
+    } else {
+      navigate(`/profile/${nextSection}`);
+    }
+  };
+
+  // Build UI
+  const experienceLevelCards = experienceLevels.map((skill) => {
+    return (
+      <SkillCard
+        key={skill.code}
+        skill={skill}
+        handler={handleExperienceLevelUpdate}
+      />
+    );
+  });
+
+  const toolCards = tools.map((skill) => {
+    return (
+      <SkillCard
+        key={skill.code}
+        skill={skill}
+        handler={handleToolsUpdate}
+      />
+    );
+  });
+
+  const developmentCards = development.map((skill) => {
+    return (
+      <SkillCard
+        key={skill.code}
+        skill={skill}
+        handler={handleDevelopmentUpdate}
+      />
+    );
+  });
+
+  const skillLevelLabels = skillLevels.map((level) => {
+    return (
+      <span className="label" key={level.level}>
+        {level.description}
+      </span>
+    );
+  });
+
   return (
     <ProfileLayout>
-      <div>Design Skills</div>
+      <div className="profileContentContainer">
+        <h1>
+          Tell us about your <span className="highlight">design</span> skills.
+        </h1>
+        <div className="contentCard profileCard basicInformationProfileCard">
+          <div className="cardHeadingWithNote">
+            <h2>
+              Design Skills
+            </h2>
+            <span className="note">
+              <span className="sup red">
+                *
+              </span>
+              Required
+            </span>
+          </div>
+          <div className="divider" />
+
+          {/* Experience Level */}
+
+          <section className="skillsSectionSpacing">
+            <div className="profileQuestionWrapper">
+              <p className="profileQuestion" id="previousVolunteer">
+                <span className="question">
+                  What&apos;s your experience level?<span className="red">*</span>
+                </span>
+              </p>
+              <form className="profileForm">
+                <div className="skillLevelLabels">
+                  <div className="spacer" />
+                  <div className="labels">
+                    {skillLevelLabels}
+                  </div>
+                </div>
+                <div className="skillCards">
+                  {experienceLevelCards}
+                </div>
+              </form>
+            </div>
+          </section>
+
+          {/* Tools */}
+
+          <section className="skillsSectionSpacing">
+            <div className="profileQuestionWrapper">
+              <p className="profileQuestion" id="previousVolunteer">
+                <span className="question">
+                  What experience do you have with these design tools?<span className="red">*</span>
+                </span>
+              </p>
+              <form className="profileForm">
+                <div className="skillLevelLabels">
+                  <div className="spacer" />
+                  <div className="labels">
+                    {skillLevelLabels}
+                  </div>
+                </div>
+                <div className="skillCards">
+                  {toolCards}
+                </div>
+              </form>
+            </div>
+          </section>
+
+          {/* Tools */}
+
+          <section>
+            <div className="profileQuestionWrapper">
+              <p className="profileQuestion" id="previousVolunteer">
+                <span className="question">
+                  What development experience do you have?<span className="red">*</span>
+                </span>
+              </p>
+              <form className="profileForm">
+                <div className="skillLevelLabels">
+                  <div className="spacer" />
+                  <div className="labels">
+                    {skillLevelLabels}
+                  </div>
+                </div>
+                <div className="skillCards">
+                  {developmentCards}
+                </div>
+              </form>
+            </div>
+          </section>
+        </div>
+
+        {/* Button Controls */}
+
+        <div className="controls">
+          <div className="buttonContainer">
+            <StandardButton
+              label="Next"
+              handler={handleNext}
+              disabled={processing}
+            />
+          </div>
+          <div className="buttonContainer spacing">
+            <StandardButton
+              theme="secondary"
+              label="Back"
+              handler={handleBack}
+              disabled={processing}
+            />
+          </div>
+        </div>
+      </div>
     </ProfileLayout>
   );
 };
