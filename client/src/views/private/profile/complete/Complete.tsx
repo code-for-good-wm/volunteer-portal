f80d9aba9ae6b72687297f608625ef6a45c334fc
Sloth I <github@slothwerks.com>
2022-06-21 10:43:23 -0400

    Added content for profile completion screen; still need to add styling for desktop

--- client/src/views/private/profile/complete/Complete.tsx
+++ client/src/views/private/profile/complete/Complete.tsx
@@ -1,8 +1,57 @@
+import { Button } from '@mui/material';
 import React from 'react';
 
+import DiscoBall from '../../../../assets/images/disco-ball.png';
+
+import StandardButton from '../../../../components/buttons/StandardButton';
+
 const ProfileComplete = () => {
+  const handleButton = () => {
+    // TODO: Would we want this button to go back to the landing page?
+    window.location.href = 'https://codeforgoodwm.org/';
+  };
+
+  // Build UI
+  const contentCard = (
+    <div className="contentCard profileCompletionCard">
+      <p className="completionThanks">
+        Thanks a bunch!
+      </p>
+      <h2 className="completionHeading">
+        Your registration is complete!
+      </h2>
+      <div className="completionDivider" />
+      <p className="center">
+        Keep your eyes peeled for a confirmation email; we&apos;ll be reviewing volunteer applications shortly.  In the meantime, keep an eye out for upcoming emails from us!
+      </p>
+    </div>
+  );
+
   return (
-    <div>Profile Complete</div>
+    <div className="viewContainer">
+      <div className="gutters">
+        <div className="completionImageContainer">
+          <img src={DiscoBall} alt="Sparkling disco ball" />
+        </div>
+        {contentCard}
+        <div className="profileCompletionButtonContainer">
+          <Button
+            color="primary"
+            disableElevation
+            fullWidth
+            variant="contained"
+            href="https://codeforgoodwm.org/"
+            sx={{
+              height: 56
+            }}
+          >
+            <span className="standardButtonText primary center">
+              Continue to Code for Good&apos;s Home Page
+            </span>
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 };
 
